994e6d1a1bd87f73ca13038ccc17d7480fb19a44
Man Cheng <cyrilcheng@protonmail.com>
2022-08-23 18:03:49 -0700

    commit tree 2aac6a7efbd89c1a1bb0c02d2a174640d65051b6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers){
+return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers){
+return drivers.slice(-2);
+}
+
+
+const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier (num){
+    return function (fare){
+        return fare * num;
+    }
+}
+
+
+const fareDoubler = function(createFareMultiplier){
+    return createFareMultiplier * 2;
+}
+
+const fareTripler = function(createFareMultiplier){
+    return createFareMultiplier * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers,fn){
+    return fn(arrayOfDrivers);
+}
